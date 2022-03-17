10c268af79d179e3bc1dddf89aabc74154c78a64
Steve Alomia <stevealomia@yahoo.com>
2022-03-17 13:12:07 -0400

    commit tree b4d5e94fdbc93af26683fa03c7b8eb4391f6d708

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(" ")
+    let capWords = words.map(word => 
+      word.charAt(0).toUpperCase() + word.slice(1) 
+  )
+  return capWords.join(" ");
+  })
 }
+
